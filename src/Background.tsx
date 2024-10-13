d2feec999d1749c6f25be2e94757e0de51d82c01
Md Samer Ansari <115770126+mrsamirr@users.noreply.github.com>
2024-10-13 16:25:07 +0530

    initialize Remotion project with ESLint, Prettier, Tailwind CSS, and basic components

---
+++ src/Background.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { AbsoluteFill } from "remotion";
+
+
+export const Background: React.FC = () => {
+    return (
+    <AbsoluteFill
+     style={{
+        // background: "linear-gradient(to right, #0f172a, #374151)"
+     }}
+     />
+    );
+
+};
---
+++ src/Root.tsx
@@ -0,0 +1,18 @@
+import './tailwind.css';
+import { Composition } from "remotion";
+import { MyComposition } from "./Composition";
+
+export const RemotionRoot: React.FC = () => {
+  return (
+    <>
+      <Composition
+        id="MyComp"
+        component={MyComposition}
+        durationInFrames={60}
+        fps={30}
+        width={1920}
+        height={1080}
+      />
+    </>
+  );
+};
